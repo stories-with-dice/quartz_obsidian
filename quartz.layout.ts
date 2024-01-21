1ccc6c64dabe94d1ea3ba89c2c2b8da2b74e5b42
dice hacker <rockguycris@gmail.com>
2024-01-21 21:42:48 +0100

    remove right side layout elements

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -74,8 +74,8 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     // Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
   ],
 }
 
